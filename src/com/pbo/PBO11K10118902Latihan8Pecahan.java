8b6e90f1d52ee890c86b5172531dd220edd3e6b0
Agung Sepruloh <agungsepruloh1996@gmail.com>
2019-09-24 18:55:54 +0700

    Initial commit

---
+++ src/com/pbo/PBO11K10118902Latihan8Pecahan.java
@@ -0,0 +1,24 @@
+package com.pbo;
+
+/**
+ *
+ * # Author
+ * Nama : Agung Sepruloh
+ * Kelas : PBO11K
+ * NIM : 10118902
+ * Deskripsi Program : Program ini berisi macam-macam tipe data penampung nilai integer
+ *
+ */
+
+public class PBO11K10118902Latihan8Pecahan {
+
+    public static void main(String[] args) {
+	    float f;
+	    double d;
+	    f = 1234567890.123456789F;
+	    d = 1_234_567_890.123456789;
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+    }
+
+}
